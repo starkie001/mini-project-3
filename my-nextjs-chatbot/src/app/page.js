d41c54aab9a576912bbf6062d1a49debd5bc851e
starkie001 <starkwanos@hotmail.com>
2025-09-12 16:57:12 +1200

    add chatbot.css styles

--- my-nextjs-chatbot/src/app/page.js
+++ my-nextjs-chatbot/src/app/page.js
@@ -1,6 +1,7 @@
 "use client";
 //import Image from "next/image";
 //import styles from "./page.module.css";
+import styles from "./chatbot.css";
 import { formatResult } from "../lib/utils/formatResults.js";
 
 import { useState } from "react";
@@ -24,15 +25,18 @@ export default function Home() {
     //TODO: Make your UI look better by adding additional elements and styles as you wish! Possibly add your styles to a new css file called chatbot.css, and import that file in this page
     <main style={{ padding: "2rem" }}>
       <h1>My Next.js Chatbot</h1>
-      <textarea
-        rows="5"
-        cols="50"
-        value={prompt}
-        onChange={(e) => setPrompt(e.target.value)}
+      <div className="chatbotContainer">
+        <textarea
+          rows="5"
+          cols="50"
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
         placeholder="Type your prompt..."
       />
       <br />
       <button onClick={sendPrompt}>Generate</button>
+      </div>
+
       <pre>{result}</pre>
     </main>
   );
